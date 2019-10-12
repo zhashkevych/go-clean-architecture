9aab562684bdc28507532aff9eb3e7c475469da2
Zhashkevych <macbook@MACBOOKs-MacBook-Air.local>
2019-10-12 16:54:23 +0300

    covered auth middleware with unit tests

--- auth/delivery/http/middleware_test.go
+++ auth/delivery/http/middleware_test.go
@@ -1,1 +1,50 @@
 package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/zhashkevych/go-clean-architecture/auth"
+	"github.com/zhashkevych/go-clean-architecture/auth/usecase"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAuthMiddleware(t *testing.T) {
+	r := gin.Default()
+	uc := new(usecase.AuthUseCaseMock)
+
+	r.POST("/api/endpoint", NewAuthMiddleware(uc), func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	w := httptest.NewRecorder()
+
+	// No Auth Header request
+	req, _ := http.NewRequest("POST", "/api/endpoint", nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+
+	// Empty Auth Header request
+	w = httptest.NewRecorder()
+
+	req.Header.Set("Authorization", "")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+
+	// Bearer Auth Header with no token request
+	w = httptest.NewRecorder()
+	uc.On("ParseToken", "").Return(&auth.User{}, auth.ErrInvalidAccessToken)
+
+	req.Header.Set("Authorization", "Bearer ")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusUnauthorized, w.Code)
+
+	// Valid Auth Header
+	w = httptest.NewRecorder()
+	uc.On("ParseToken", "token").Return(&auth.User{}, nil)
+
+	req.Header.Set("Authorization", "Bearer token")
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
