75a9072a93ffac61330df99e8a59a4ea289fa947
Zhashkevych <macbook@MACBOOKs-MacBook-Air.local>
2019-10-12 16:28:23 +0300

    fixed auth token parsing; unit test for auth usecase added

--- auth/usecase/usecase.go
+++ auth/usecase/usecase.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/sha1"
 	"fmt"
+	"time"
 
 	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/zhashkevych/go-clean-architecture/auth"
@@ -15,16 +16,22 @@ type AuthClaims struct {
 }
 
 type AuthUseCase struct {
-	userRepo   auth.UserRepository
-	hashSalt   string
-	signingKey []byte
+	userRepo       auth.UserRepository
+	hashSalt       string
+	signingKey     []byte
+	expireDuration time.Duration
 }
 
-func NewAuthUseCase(userRepo auth.UserRepository, hashSalt string, signingKey []byte) *AuthUseCase {
+func NewAuthUseCase(
+	userRepo auth.UserRepository,
+	hashSalt string,
+	signingKey []byte,
+	tokenTTLSeconds time.Duration) *AuthUseCase {
 	return &AuthUseCase{
-		userRepo:   userRepo,
-		hashSalt:   hashSalt,
-		signingKey: signingKey,
+		userRepo:       userRepo,
+		hashSalt:       hashSalt,
+		signingKey:     signingKey,
+		expireDuration: time.Second * tokenTTLSeconds,
 	}
 }
 
@@ -54,6 +61,9 @@ func (a *AuthUseCase) SignIn(ctx context.Context, username, password string) (st
 
 	claims := AuthClaims{
 		User: user,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(a.expireDuration)),
+		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -62,7 +72,7 @@ func (a *AuthUseCase) SignIn(ctx context.Context, username, password string) (st
 }
 
 func (a *AuthUseCase) ParseToken(ctx context.Context, accessToken string) (*auth.User, error) {
-	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &AuthClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -73,7 +83,7 @@ func (a *AuthUseCase) ParseToken(ctx context.Context, accessToken string) (*auth
 		return nil, err
 	}
 
-	if claims, ok := token.Claims.(AuthClaims); ok && token.Valid {
+	if claims, ok := token.Claims.(*AuthClaims); ok && token.Valid {
 		return claims.User, nil
 	}
 
--- auth/usecase/usecase_test.go
+++ auth/usecase/usecase_test.go
@@ -1,1 +1,43 @@
 package usecase
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/zhashkevych/go-clean-architecture/auth"
+	"github.com/zhashkevych/go-clean-architecture/auth/repository/mock"
+	"testing"
+)
+
+func TestAuthFlow(t *testing.T) {
+	repo := new(mock.UserStorageMock)
+
+	uc := NewAuthUseCase(repo, "salt", []byte("secret"), 86400)
+
+	var (
+		username = "user"
+		password = "pass"
+
+		ctx = context.Background()
+
+		user = &auth.User{
+			Username: username,
+			Password: "11f5639f22525155cb0b43573ee4212838c78d87", // sha1 of pass+salt
+		}
+	)
+
+	// Sign Up
+	repo.On("CreateUser", user).Return(nil)
+	err := uc.SignUp(ctx, username, password)
+	assert.NoError(t, err)
+
+	// Sign In (Get Auth Token)
+	repo.On("GetUser", user.Username, user.Password).Return(user, nil)
+	token, err := uc.SignIn(ctx, username, password)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+
+	// Verify token
+	parsedUser, err := uc.ParseToken(ctx, token)
+	assert.NoError(t, err)
+	assert.Equal(t, user, parsedUser)
+}
